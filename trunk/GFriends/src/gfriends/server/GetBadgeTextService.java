4301444086173151013d1aa6c35f61490b00ef52
jalen.cn@gmail.com <jalen.cn@gmail.com@1e6bd584-00aa-11de-87c9-111d11f6bf44>
2010-09-01 08:53:17 +0000

    bug fixs

--- trunk/GFriends/src/gfriends/server/GetBadgeTextService.java
+++ trunk/GFriends/src/gfriends/server/GetBadgeTextService.java
@@ -3,6 +3,8 @@ package gfriends.server;
 import gfriends.server.model.Greeting;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -15,42 +17,47 @@ public class GetBadgeTextService extends javax.servlet.http.HttpServlet {
 
   private static final long serialVersionUID = 4183788899241319346L;
 
+  public void doPost(HttpServletRequest resuest, HttpServletResponse response) throws IOException {
+    doGet(resuest, response);
+  }
+
   public void doGet(HttpServletRequest resuest, HttpServletResponse response) throws IOException {
 
     response.setContentType("text/javascript");
 
-    String param = resuest.getParameter("lastCheckout");
+    String lastCheckout = resuest.getParameter("lastCheckout");
 
-    if (param != null && param.trim().length() > 0) {
+    Date lastCheckoutDt = null;
 
-      long lastCheckout = Long.parseLong(param);
-
+    if (lastCheckout != null && lastCheckout.trim().length() > 0) {
       Calendar calendar = Calendar.getInstance();
-      calendar.setTimeInMillis(lastCheckout);
-      Date lastCheckoutDt = calendar.getTime();
-
-      PersistenceManager pm = null;
-      try {
-        pm = PMF.get().getPersistenceManager();
-
-        String query = "select from " + Greeting.class.getName() + " where date > :lastCheckoutDt order by date desc";
-
-        @SuppressWarnings("unchecked")
-        List<Greeting> greetings = (List<Greeting>) pm.newQuery(query).execute(lastCheckoutDt);
-        
-        if(greetings.size() > 0){
-          StringBuilder sb = new StringBuilder();
-          sb.append("{\"size\":").append(greetings.size()).append(", \"newLastCheckout\":")
-              .append(greetings.get(greetings.size() - 1).getDate().getTime()).append("}");
-  
-          response.getOutputStream().print(sb.toString());
-        }else{
-          response.getOutputStream().print("{\"size\":0, \"newLastCheckout\":" + lastCheckout + "}");
-        }
-        
-      } finally {
-        pm.close();
-      }
+      calendar.setTimeInMillis(Long.valueOf(lastCheckout));
+      lastCheckoutDt = calendar.getTime();
+    } else {
+      lastCheckoutDt = new Date(0);
     }
+    
+    PersistenceManager pm = null;
+    try {
+      pm = PMF.get().getPersistenceManager();
+
+      String query = "select from " + Greeting.class.getName() + " where date > :lastCheckoutDt order by date desc";
+
+      @SuppressWarnings("unchecked")
+      List<Greeting> greetings = (List<Greeting>) pm.newQuery(query).execute(lastCheckoutDt);
+
+      if (greetings.size() > 0) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{\"size\":").append(greetings.size()).append(", \"newLastCheckout\":").append(greetings.get(0).getDate().getTime()).append("}");
+
+        response.getOutputStream().print(sb.toString());
+      } else {
+        response.getOutputStream().print("{\"size\":0, \"newLastCheckout\":" + lastCheckout + "}");
+      }
+
+    } finally {
+      pm.close();
+    }
+
   }
 }
