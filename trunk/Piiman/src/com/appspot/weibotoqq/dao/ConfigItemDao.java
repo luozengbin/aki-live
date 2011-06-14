2ed51b792fbeca4165b5817fcc4904a3f64c12c0
jalen.cn@gmail.com <jalen.cn@gmail.com@1e6bd584-00aa-11de-87c9-111d11f6bf44>
2011-06-14 14:52:40 +0000

    腾讯微博WebAPI调用的重构

--- trunk/Piiman/src/com/appspot/weibotoqq/dao/ConfigItemDao.java
+++ trunk/Piiman/src/com/appspot/weibotoqq/dao/ConfigItemDao.java
@@ -1,6 +1,8 @@
 package com.appspot.weibotoqq.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.jdo.PersistenceManager;
 
@@ -11,7 +13,8 @@ public class ConfigItemDao {
   /**
    * DEFINATION OF QUERY
    */
-  private static final String query = "select from " + ConfigItem.class.getName() + " where key == :key";
+  private static final String QL_001 = "select from " + ConfigItem.class.getName() + " where key == :key";
+  private static final String QL_002 = "select from " + ConfigItem.class.getName();
 
   private PersistenceManager pm = null;
 
@@ -22,11 +25,39 @@ public class ConfigItemDao {
   public String getValue(String key) {
     try {
       pm = PMF.get().getPersistenceManager();
-      
-      @SuppressWarnings("unchecked")
-      List<ConfigItem> contactList = (List<ConfigItem>) pm.newQuery(query).execute(key);
 
-      return (contactList != null && contactList.size() > 0) ? contactList.get(0).getValue() : null;
+      @SuppressWarnings("unchecked")
+      List<ConfigItem> configItemList = (List<ConfigItem>) pm.newQuery(QL_001).execute(key);
+
+      return (configItemList != null && configItemList.size() > 0) ? configItemList.get(0).getValue() : null;
+
+    } finally {
+      if (pm != null) {
+        pm.close();
+        pm = null;
+      }
+    }
+  }
+
+  public Map<String, String> getValues() {
+
+    Map<String, String> values = new HashMap<String, String>();
+
+    try {
+      pm = PMF.get().getPersistenceManager();
+
+      @SuppressWarnings("unchecked")
+      List<ConfigItem> configItemList = (List<ConfigItem>) pm.newQuery(QL_002).execute();
+
+      if (configItemList != null) {
+
+        for (ConfigItem configItem : configItemList) {
+          values.put(configItem.getKey(), configItem.getValue());
+        }
+
+      }
+
+      return values;
 
     } finally {
       if (pm != null) {
