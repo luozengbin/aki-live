b818645ef1c96c22ccd64709e683c36359f6e20c
jalen.cn@gmail.com <jalen.cn@gmail.com@1e6bd584-00aa-11de-87c9-111d11f6bf44>
2011-06-30 14:39:39 +0000

    解决tqq 签名错误问题

---
+++ trunk/piment/src/com/appspot/piment/jobs/Job1002.java
@@ -0,0 +1,34 @@
+package com.appspot.piment.jobs;
+
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.appspot.piment.Constants;
+
+public class Job1002 extends HttpServlet {
+
+  private static final long serialVersionUID = 8080410909511203717L;
+  private static final Logger log = Logger.getLogger(Constants.FQCN + Job1002.class.getName());
+
+  public Job1002() {
+	super();
+  }
+
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+	log.info("-- job1001 start --");
+	try {
+
+
+	  
+	  log.info("-- job1001 end --");
+
+	} catch (Exception e) {
+	  throw new RuntimeException(e);
+	}
+
+  }
+}
